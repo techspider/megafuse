ddad336004ade88133ede7fc9160dda9ece1d5fa
mr_chainman <rvreman7@me.com>
2019-02-05 11:56:18 +0400

    API Call Update

--- MegaFUSEUI/CPanels/LogInPanel.Designer.cs
+++ MegaFUSEUI/CPanels/LogInPanel.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.logInLbl = new System.Windows.Forms.Label();
             this.uiDescriptionLabel = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.usernameInput = new System.Windows.Forms.TextBox();
+            this.passwordInput = new System.Windows.Forms.TextBox();
             this.unameUilabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.loginBtn = new System.Windows.Forms.Button();
@@ -60,19 +60,20 @@
             this.uiDescriptionLabel.TabIndex = 1;
             this.uiDescriptionLabel.Text = "Enter your username and password.";
             // 
-            // textBox1
+            // usernameInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(27, 193);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(344, 20);
-            this.textBox1.TabIndex = 2;
+            this.usernameInput.Location = new System.Drawing.Point(27, 193);
+            this.usernameInput.Name = "usernameInput";
+            this.usernameInput.Size = new System.Drawing.Size(344, 20);
+            this.usernameInput.TabIndex = 2;
             // 
-            // textBox2
+            // passwordInput
             // 
-            this.textBox2.Location = new System.Drawing.Point(27, 250);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(344, 20);
-            this.textBox2.TabIndex = 3;
+            this.passwordInput.Location = new System.Drawing.Point(27, 250);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.Size = new System.Drawing.Size(344, 20);
+            this.passwordInput.TabIndex = 3;
+            this.passwordInput.UseSystemPasswordChar = true;
             // 
             // unameUilabel
             // 
@@ -123,8 +124,8 @@
             this.Controls.Add(this.loginBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.unameUilabel);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.passwordInput);
+            this.Controls.Add(this.usernameInput);
             this.Controls.Add(this.uiDescriptionLabel);
             this.Controls.Add(this.logInLbl);
             this.Name = "LogInPanel";
@@ -139,8 +140,8 @@
 
         private System.Windows.Forms.Label logInLbl;
         private System.Windows.Forms.Label uiDescriptionLabel;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox usernameInput;
+        private System.Windows.Forms.TextBox passwordInput;
         private System.Windows.Forms.Label unameUilabel;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button loginBtn;
--- MegaFUSEUI/CPanels/LogInPanel.cs
+++ MegaFUSEUI/CPanels/LogInPanel.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static MegaFUSE.UI.MGFuseMain;
+using CG.Web.MegaApiClient;
 
 namespace MegaFUSEUI.CPanels
 {
@@ -24,6 +26,21 @@ namespace MegaFUSEUI.CPanels
 
         private void loginBtn_Click(object sender, EventArgs e)
         {
+            MegaClient = new MegaApiClient();
+            try
+            {
+                MegaClient.Login(usernameInput.Text, passwordInput.Text);
+            }
+            catch(ApiException ex)
+            {
+                MessageBox.Show("API Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed logging in! Please try again later.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
         }
     }
