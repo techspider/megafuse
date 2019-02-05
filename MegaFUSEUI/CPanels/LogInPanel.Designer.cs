6e78dc4e31bd49f147bcca29a81d626de1d41191
mr_chainman <rvreman7@me.com>
2019-02-05 11:52:45 +0400

    Finish Logon UI

--- MegaFUSEUI/CPanels/LogInPanel.Designer.cs
+++ MegaFUSEUI/CPanels/LogInPanel.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.logInLbl = new System.Windows.Forms.Label();
             this.uiDescriptionLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.unameUilabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.quitbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // logInLbl
@@ -54,17 +60,78 @@
             this.uiDescriptionLabel.TabIndex = 1;
             this.uiDescriptionLabel.Text = "Enter your username and password.";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 193);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 250);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(344, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // unameUilabel
+            // 
+            this.unameUilabel.AutoSize = true;
+            this.unameUilabel.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.unameUilabel.Location = new System.Drawing.Point(23, 171);
+            this.unameUilabel.Name = "unameUilabel";
+            this.unameUilabel.Size = new System.Drawing.Size(80, 19);
+            this.unameUilabel.TabIndex = 4;
+            this.unameUilabel.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label1.Location = new System.Drawing.Point(23, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Password";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(305, 449);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(66, 23);
+            this.loginBtn.TabIndex = 6;
+            this.loginBtn.Text = "Log In";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // quitbtn
+            // 
+            this.quitbtn.Location = new System.Drawing.Point(233, 449);
+            this.quitbtn.Name = "quitbtn";
+            this.quitbtn.Size = new System.Drawing.Size(66, 23);
+            this.quitbtn.TabIndex = 7;
+            this.quitbtn.Text = "Quit";
+            this.quitbtn.UseVisualStyleBackColor = true;
+            this.quitbtn.Click += new System.EventHandler(this.quitbtn_Click);
+            // 
             // LogInPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.quitbtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.unameUilabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.uiDescriptionLabel);
             this.Controls.Add(this.logInLbl);
             this.Name = "LogInPanel";
             this.Padding = new System.Windows.Forms.Padding(25);
             this.Size = new System.Drawing.Size(400, 500);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +139,11 @@
 
         private System.Windows.Forms.Label logInLbl;
         private System.Windows.Forms.Label uiDescriptionLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label unameUilabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button quitbtn;
     }
 }
--- MegaFUSEUI/CPanels/LogInPanel.cs
+++ MegaFUSEUI/CPanels/LogInPanel.cs
@@ -16,5 +16,15 @@ namespace MegaFUSEUI.CPanels
         {
             InitializeComponent();
         }
+
+        private void quitbtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
