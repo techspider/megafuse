007f45c6ebe640e38be5c8531fbc86e28e18a6b3
mr_chainman <rvreman7@me.com>
2019-02-05 11:45:29 +0400

    Add MegaFUSE core library

---
+++ MegaFUSE.UI/Controls/TextInputBox.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MegaFUSE.UI.Controls
+{
+    partial class TextInputBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputControl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputControl
+            // 
+            this.inputControl.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputControl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(0)))), ((int)(((byte)(7)))));
+            this.inputControl.Location = new System.Drawing.Point(5, 5);
+            this.inputControl.Multiline = true;
+            this.inputControl.Name = "inputControl";
+            this.inputControl.Size = new System.Drawing.Size(173, 29);
+            this.inputControl.TabIndex = 0;
+            // 
+            // TextInputBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.inputControl);
+            this.Name = "TextInputBox";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Size = new System.Drawing.Size(183, 39);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputControl;
+    }
+}
---
+++ MegaFUSE/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MegaFUSE")]
+[assembly: AssemblyDescription("MegaFUSE Core Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("mr_chainman (techspider)")]
+[assembly: AssemblyProduct("MegaFUSE")]
+[assembly: AssemblyCopyright("Copyright © mr_chainman (techspider) 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a81eb38-d0b7-456c-a0f7-f8dd70d3930b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
